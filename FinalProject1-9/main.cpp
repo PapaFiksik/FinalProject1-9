f915f4f3f10faf992ba8bbc53d14e88206a2c894
Grigorii <prozorov1@mail.ru>
2022-06-11 21:34:42 +0300

    Добавьте файлы проекта.

---
+++ FinalProject1-9/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "");
+	bool work = true;
+
+	while (work) {
+		cout << "1- Вход. 2 - Регистрация. 3 - Справка. 0 - Выход." << endl;
+		short userChoice;
+		cin >> userChoice;
+
+		switch (userChoice)
+		{
+		case 1:
+			cout << "Вход" << endl;
+			break;
+
+		case 2:
+			cout << "Регистрация" << endl;
+			break;
+
+		case 3:
+			cout << "Справка" << endl;
+			break;
+
+		case 0:
+			cout << "Выход" << endl;
+			work = false;
+			break;
+		default:
+			cout << "Введено неверное значение." << endl;
+			break;
+		}
+
+	}
+	return 0;
+}
